f3c12c154e0a3c69a6f295e41ee07ce0bd3a13e3
Sergey Skvortsov <19serhiy96@gmail.com>
2016-08-02 12:58:43 +0300

    svg
    fixed problem with svg

--- js/jscript.js
+++ js/jscript.js
@@ -21,10 +21,8 @@ $(document).ready(function() {
 
 	function onScroll(){
        	if ($(window).scrollTop() < 200) {
-       		console.log('scrollTop < 200');
             $('.navbar').fadeIn(300);
         } else {
-       		console.log('scrollTop > 200');
             $('.navbar').fadeOut(300);
         }
 	}
@@ -92,4 +90,23 @@ $(document).ready(function() {
             $('#scroll-top').fadeOut(300);
         };
 	});
+	
+	$(window).on('resize', polylineSize);
+
+	function polylineSize() {
+		var dot1 = $('.simple').offset().left + $('.simple').width() / 2;
+		var dot2 = $('.cost').offset().left + $('.cost').width() / 2;
+		var dot3 = $('.innovative').offset().left + $('.innovative').width() / 2;
+		var dot4 = $('.all-in').offset().left + $('.all-in').width() / 2;
+		var string  = 'M 0 0 L ' +  dot1 + ' 45.497 L ' + dot2 +  ' 27.497 L ' + dot3 + ' 43.497 L ' + dot4 +  ' 27.497 L ' + $(window).width() + ' 57.497';
+		$('.path').attr('d', string);
+
+		$('.circle1').attr('cx', dot1);
+		$('.circle2').attr('cx', dot2);
+		$('.circle3').attr('cx', dot3);
+		$('.circle4').attr('cx', dot4);
+	};
+
+	polylineSize();
+				
 });
